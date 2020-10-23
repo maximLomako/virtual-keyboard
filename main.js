3824b8290b88493426aba4a9069585672cb8bc42
maximLomako <maxim.lomako@icloud.com>
2020-10-24 01:38:16 +0300

    feat: add dates block, add function clear input

--- main.js
+++ main.js
@@ -39,14 +39,66 @@ function setBgGreet() {
     }
   }
 
-// Get Name
 function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
-  } else {
-    name.textContent = localStorage.getItem('name');
-  }
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Input Your]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
 }
+
+function setName(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.key === 'Enter' && e.target.innerText !== '') {
+            localStorage.setItem('name', e.target.innerText);
+            name.blur();
+        }
+        if (e.key === 'Enter' && e.target.innerText === '') {
+            name.blur();
+            getName()
+        }
+    }else if (e.type === 'click'){
+        e.target.innerText = '';
+        name.focus();
+    } else {
+        getName()
+    }
+}
+
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = '[Input Your Goal!]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+function setFocus(e) {
+    if (e.type === 'keypress') {
+        // Make sure enter is pressed
+        if (e.key === 'Enter' && e.target.innerText !== '') {
+            localStorage.setItem('focus', e.target.innerText);
+            focus.blur();
+        }
+        if (e.key === 'Enter' && e.target.innerText === '') {
+            focus.blur();
+            getFocus()
+        }
+    }else if (e.type === 'click'){
+        e.target.innerText = '';
+        focus.focus();
+    } else {
+        getFocus()
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+name.addEventListener('click', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+focus.addEventListener('click', setFocus);
 
 
 
@@ -70,49 +122,6 @@ function showDate() {
 // Run
 showTime();
 setBgGreet();
+showDate();
 getName();
 getFocus();
-showDate();
-
-// set name
-function setName(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
-      name.blur();
-    }
-  } else {
-    localStorage.setItem('name', e.target.innerText)
-  }
-}
-
-
-// Get Name
-function getFocus() {
-  if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Focus]';
-  } else {
-    focus.textContent = localStorage.getItem('focus');
-  }
-}
-
-// set focus
-function setFocus(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      e.preventDefault();
-      localStorage.setItem('focus', e.target.innerText);
-      focus.blur();
-    }
-  } else {
-    localStorage.setItem('focus', e.target.innerText)
-  }
-}
-
-name.addEventListener('keyPress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keyPress', setFocus);
-focus.addEventListener('blur', setFocus);
-
