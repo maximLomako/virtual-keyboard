e08bf7f11ec63db3c8320c05cc9c295433c0b67f
maximLomako <maxim.lomako@icloud.com>
2020-10-30 15:01:42 +0300

    feat: add shift func

--- virtual-keyboard/main.js
+++ virtual-keyboard/main.js
@@ -3,26 +3,21 @@ const keyLayoutEng = [
   "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
   "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "enter",
   "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/",
-  "showKeyboard", "voice", "lang", "space", "arrrowLeft", "arrowRight"
+  "showKeyboard", "voice", "lang", "space", "arrowLeft", "arrowRight"
 ];
 
 const keyLayoutRus = [
-  "]", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
+  "˜", "!", "@", "#", "$", "%", "ˆ", "&", "*", "(", ")", "_", "+","backspace",
   "tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ",
   "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "enter",
   "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
-  "showKeyboard", "voice", "lang", "space", "arrrowLeft", "arrowRight"
+  "showKeyboard", "voice", "lang", "space", "arrowLeft", "arrowRight"
 ]
 
-const keyLayoutShifted = [
-  "˜", "!", "@", "#", "$", "%", "6", "&", "*", "(", ")", "_", "+", "backspace",
-  "tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ",
-  "caps", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "enter",
-  "shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".",
-  "showKeyboard", "voice", "lang", "space", "arrrowLeft", "arrowRight"
-]
-
-const body = document.querySelector('body');
+const body = document.querySelector('body'),
+  textarea = document.querySelector('textarea');
+ let capsLock = false,
+  shift = false;
 
 const initKeyBoard = () => {
 
@@ -36,7 +31,7 @@ const initKeyBoard = () => {
   keyboard.appendChild(keys);
 
   keys.insertAdjacentHTML('afterbegin', keyLayoutEng.map((key, i) => `
-      <div class="key">
+      <div class="key" dataValue=${key.toLowerCase()}>
         <span class="key__item" dataValue=${keyLayoutRus[i]}>${keyLayoutRus[i]}</span>
         <strong class="key__main-item" dataValue=${key}>${key}</strong> 
       </div>
@@ -66,8 +61,6 @@ const initKeyBoard = () => {
         break;
       case "tab":
         element.classList.add('key--medium__width')
-      case "done":
-        element.classList.add('key--medium__width')
         break;
       case "shift":
         element.classList.add('key--medium__width')
@@ -86,12 +79,138 @@ const openCloseKeyboard = () => {
   btn[1].addEventListener('click', () => {
     keyboard.classList.toggle('keyboard--active');
   })
+
+  document.addEventListener('click', (e) => {
+    if (e.target === body) {
+      keyboard.classList.toggle('keyboard--active');
+    }
+  })
 }
 openCloseKeyboard();
 
-document.addEventListener('click', (e)=> {
-  const keyboard = document.querySelector('.keyboard');
-  if (e.target === body) {
-    keyboard.classList.toggle('keyboard--active');
+const inputItemFromKeyboard = () => {
+
+  const capsBtn = document.querySelectorAll(("[dataValue='caps']"))[0];
+  const shiftBtn = document.querySelectorAll(("[dataValue='shift']"))[0];
+
+  capsBtn.addEventListener('click', () => {
+    capsLock = !capsLock;
+    capsBtn.classList.toggle('key--activeAlways');
+  })
+
+  shiftBtn.addEventListener('click', () => {
+    shift = !shift;
+    shiftBtn.classList.toggle('key--activeAlways');
+  })
+
+  const key = document.querySelectorAll(".key");
+  key.forEach(element => element.addEventListener('mouseover', (e) => {
+    const target = e.currentTarget;
+    if (target === element) {
+      element.classList.add('key--active')
+    }
+  }))
+
+  key.forEach(element => element.addEventListener('mouseout', (e) => {
+    const target = e.currentTarget;
+    if (target === element) {
+      element.classList.remove('key--active')
+    }
+  }))
+
+  key.forEach((element,i) => element.addEventListener('click', (e) => {
+    const target = e.currentTarget;
+    if (target === element) {
+      switch (element.getAttribute('dataValue')) {
+        case "backspace":
+          textarea.value = textarea.value.slice(0, -1)
+          break;
+        case "enter":
+          textarea.value += "\n"
+          break;
+        case "caps":
+          textarea.value += '';
+          break;
+        case "tab":
+          textarea.value += '';
+          break;
+        case "shift":
+          textarea.value += ""
+          break;
+        case "space":
+          textarea.value += " "
+          break;
+        case "voice":
+          textarea.value += ""
+          break;
+        case "lang":
+          textarea.value += ""
+          break;
+        case "arrowleft":
+          textarea.value += ""
+          break;
+        case "arrowright":
+          textarea.value += ""
+          break;
+        case "showkeyboard":
+          textarea.value += ""
+          break;
+        default:
+          if (capsLock && shift) {
+            switchShiftKey(keyItemValue);
+            textarea.value += element.getAttribute('dataValue').toLowerCase();
+             
+          }
+          if (!capsLock && shift) {
+            switchShiftKey(keyItemValue);
+            textarea.value += element.getAttribute('dataValue').toUpperCase();
+          }
+          if (capsLock && !shift) {
+            switchShiftKey(keyMainItemValue);
+            textarea.value += element.getAttribute('dataValue').toUpperCase();
+          }
+          if (!capsLock && !shift) {
+            switchShiftKey(keyMainItemValue);
+            textarea.value += element.getAttribute('dataValue').toLowerCase();
+          }
+          break;
+      }
+    }
+  }))
+
+}
+inputItemFromKeyboard();
+
+
+const keyValue = document.querySelectorAll('.key');
+const keyMainItemValue = document.querySelectorAll('.key__main-item');
+let keyItemValue = document.querySelectorAll('.key__item');
+const reg = /([А-Я, A-Z])/i;
+
+const switchShiftKey = (fakeArr) => {
+ keyValue.forEach((element, i) => {
+   if (!reg.test(keyItemValue[i].getAttribute('datavalue'))) {
+     element.setAttribute('dataValue', fakeArr[i].getAttribute('datavalue'));
+   }
+ })
+}
+
+const checkValueKey = () => {
+  if (capsLock && shift) {
+    textarea.value += element.getAttribute('dataValue').toLowerCase();
+    switchShiftKey(keyItemValue);
   }
-})
+  if (!capsLock && shift) {
+    textarea.value += element.getAttribute('dataValue').toUpperCase();
+    switchShiftKey(keyItemValue);
+  }
+  if (capsLock && !shift) {
+    textarea.value += element.getAttribute('dataValue').toUpperCase();
+    switchShiftKey(keyMainItemValue);
+
+  }
+  if (!capsLock && !shift) {
+    textarea.value += element.getAttribute('dataValue').toLowerCase();
+    switchShiftKey(keyMainItemValue);
+  }
+}
